1109e5f47643a63253402b82df7f2b15ae2dead7
sahana <>
2019-05-06 16:14:11 +0530

    no message

--- app/src/main/java/com/sahana/HorizontalCalendarExample/MainActivity.java
+++ app/src/main/java/com/sahana/HorizontalCalendarExample/MainActivity.java
@@ -3,7 +3,6 @@ package com.sahana.HorizontalCalendarExample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.sahana.horizontalcalendarview.CustomHorizontalCalendar;
